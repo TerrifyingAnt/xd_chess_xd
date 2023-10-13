1674eb89b5211a2218749f6fcac39061755b4c5c
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2023-10-14 01:50:33 +0300

    aaaaaaaaaaaaaaaaa

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,31 @@ bool whitePlays;
 LiChessApi api;
 ChessMove lastMove;
 
+String stringMove = "";
+
 String uid = "xd";
 
-int mode = 0;
-int depth = 2;
-int maxSteps = 10;
-
 MAX7219 <2, 2, 12, 14, 13> mtrx;   // одна матрица (1х1), пин CS на D5
+
+// Define the number of bits and create an array to store button states
+const int numBits = 16; // Change this to match your specific number of bits
+int buttonStates[numBits] = {1}; // Initialize all button states as not pressed (0)
+int lightningButtonStates[numBits] = {1};
+
+// Q7 pins
+int dataPin1 = 34;
+int dataPin2 = 35;
+int dataPin3 = 25;
+int dataPin4 = 33;
+
+// CE pin 15
+int clockEnablePin = 32;
+
+// CP pin 2
+int clockPin = 26;
+
+// PL pin 1
+int load = 27;
 
 bool boolPlay = true;
 
@@ -31,9 +49,9 @@ void computerMove(ChessBoard& board, int depth, int maxSteps);
 void ledForFigures(ChessBoard& board);
 void playGame();
 
-int userMove(ChessBoard& board)
+int userMove(ChessBoard& tempBoard)
 {
-    if (board.whitePlays)
+    if (tempBoard.whitePlays)
         Print("White");
     else
         Print("Black");
@@ -41,13 +59,13 @@ int userMove(ChessBoard& board)
     Println(" goes next");
 
     Println("Perform move:");
-    char buffer[12];
+    char buffer[5];
     Serial.readBytesUntil('\n', buffer, sizeof(buffer));
 
     if (strlen(buffer) == 3) {
         Print("Possible moves: ");
         ChessMove move = ChessMove(buffer);
-        board.possibleMoves(move.from).printList();
+        tempBoard.possibleMoves(move.from).printList();
         Println("");
         return 1;
     }
@@ -56,30 +74,30 @@ int userMove(ChessBoard& board)
 
     ChessMove move(buffer);
 
-    if (board.validMove(move)) {
+    if (tempBoard.validMove(move)) {
         Print("Moving ");
         char name[16];
-        board.board[move.from].name(name);
+        tempBoard.board[move.from].name(name);
         Print(name);
         Print(" ");
         move.printMove();
 
-        if (!board.board[move.to].empty()) {
+        if (!tempBoard.board[move.to].empty()) {
           Print("Taking ");
-          board.board[move.to].name(name);
+          tempBoard.board[move.to].name(name);
           Println(name);
         }
 
         Println("");
-        String stringMove = move.getMove();
+        stringMove = move.getMove();
         api.makeMove(uid, stringMove);
         board.performMove(move);
 
-        //lastMove = move;
-        //ledForFigures(board);
+        lastMove = move;
+        ledForFigures(board);
 
         Println(stringMove);
-        ledForFigures(board);
+        stringMove = "";
     } else {
         Println("Invalid move try again");
         return 1;
@@ -88,31 +106,106 @@ int userMove(ChessBoard& board)
     return 0;
 }
 
-void computerMove()
+void computerMove(ChessBoard& tempBoard)
 {
-  String response = api.getCurrentGameState(uid);
-  int index = response.lastIndexOf("\"moves\":") + 8;
-  String moves = response.substring(response.indexOf("\"", index + 4) - 4, response.indexOf("\"", index + 4));
-  Println(moves);
-  char* createMove = new char[5];
-  for(int i = 0; i < moves.length(); i++) {
-      if(i != 2) {
-        createMove[i] = moves[i];
-      }
-    else {
-      createMove[i] = ' ';
+  bool moveWasMade = false;
+  while(!moveWasMade){
+    String response = api.getCurrentGameState(uid);
+    int index = response.lastIndexOf("\"moves\":") + 8;
+    String moves = response.substring(response.indexOf("\"", index + 4) - 4, response.indexOf("\"", index + 4));
+    char buffer[] = {
+            moves[0], moves[1], ' ', moves[2], moves[3], '\0'
+    };
+    ChessMove lichessMove = ChessMove(buffer);
+    String move1 = lichessMove.getMove();
+    String move2 = lastMove.getMove();
+    if(!move1.equals(move2)) {
+      tempBoard.performMove(lichessMove);
+      moveWasMade = true;
+      lastMove = lichessMove;
+      ledForFigures(board);
+      break;
     }
   }
-  ChessMove lichessMove = ChessMove(createMove);
-  board.performMove(createMove);
-  api.cancelGameWithBot(uid);
+  //api.cancelGameWithBot(uid);
 }
+
+void shiftRegisters() {
+  digitalWrite(load, LOW);
+  digitalWrite(load, HIGH);
+  
+  bool dataChanged = false; // Flag to indicate if there are any changes in data
+  
+  Serial.print("Data from 1: ");
+  for (int i = 0; i < numBits; i++) {
+    int bit = digitalRead(dataPin1);
+    if (bit == HIGH) {
+      Serial.print("1");
+    } else {
+      Serial.print("0");
+    }
+    digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+  }
+
+  Serial.println("");
+  Serial.print("Data from 2: ");
+
+  digitalWrite(load, LOW);
+  digitalWrite(load, HIGH);
+  for (int i = 0; i < numBits; i++) {
+    int bit = digitalRead(dataPin2);
+    if (bit == HIGH) {
+      Serial.print("1");
+    } else {
+      Serial.print("0");
+    }
+    digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+  }
+  Serial.println("");
+  Serial.print("Data from 3: ");
+    digitalWrite(load, LOW);
+  digitalWrite(load, HIGH);
+  for (int i = 0; i < numBits; i++) {
+    int bit = digitalRead(dataPin3);
+    if (bit == HIGH) {
+      Serial.print("1");
+    } else {
+      Serial.print("0");
+    }
+    digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+  }
+  Serial.println("");
+
+  Serial.print("Data from 4: ");
+    digitalWrite(load, LOW);
+  digitalWrite(load, HIGH);
+  for (int i = 0; i < numBits; i++) {
+    int bit = digitalRead(dataPin4);
+    if (bit == HIGH) {
+      Serial.print("1");
+    } else {
+      Serial.print("0");
+    }
+        digitalWrite(clockPin, HIGH);
+    digitalWrite(clockPin, LOW);
+  }
+    Serial.println("");
+    Serial.println("");
+    digitalWrite(clockPin, HIGH);
+  digitalWrite(clockPin, LOW);
+  delay(1000);
+}
+
 
 void setup() {
   mtrx.setRotation(1);
+  mtrx.setFlip(true, false);
   mtrx.setType(GM_SERIES);
   delay(100);
-  mtrx.setConnection(GM_LEFT_TOP_DOWN);
+  mtrx.setConnection(GM_LEFT_BOTTOM_UP);
   delay(100);
   mtrx.begin();       // запускаем
   delay(100);
@@ -131,7 +224,15 @@ void setup() {
   //shiftRegistersThread.onRun(shiftRegisters);
   //shiftRegistersThread.setInterval(5);
 
-  WiFi.begin("IoT_Case", "qweqweqwe");
+    // Setup 74HC165 connections
+  pinMode(dataPin1, INPUT);
+  pinMode(dataPin2, INPUT);
+  pinMode(dataPin3, INPUT);
+  pinMode(dataPin4, INPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(load, OUTPUT);
+
+  WiFi.begin("TP-Link_10DC", "37163006");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -152,11 +253,12 @@ void loop() {
 
 }
 
-void ledForFigures(ChessBoard& board) {
+void ledForFigures(ChessBoard& tempBoard) {
     Serial.print("XDXD");
+    mtrx.clear();
     for(int i = 0; i < 8; i++) {
       for(int j = 0; j < 8; j++) {
-        if(!board.board[i * 8 + j].empty()) {
+        if(!tempBoard.board[i * 8 + j].empty()) {
           mtrx.rect(i * 2, j * 2, i * 2 + 1, j * 2 + 1);
         }
 
@@ -165,7 +267,11 @@ void ledForFigures(ChessBoard& board) {
     }
   }
 
+  void gerkonsToLed() {}
+
+
 void playGame() {
+  ledForFigures(board);
   while (boolPlay) {
       Print("Current board score: ");
       Println(ChessEngine::evaluateMoveScore(board));
@@ -173,15 +279,14 @@ void playGame() {
       board.printBoard();
 
       if (!board.whitePlays) {
-          computerMove();
-          board.whitePlays = !board.whitePlays;
-          //update = true;
-          continue;
+          computerMove(board);
       }
 
       int status;
       status = userMove(board);
-      if (status == 1) continue;
+      if (status == 1) {          
+        continue;
+      };
       if (status == 2) boolPlay = false;
 
       int endState = board.gameEnded();
@@ -195,5 +300,8 @@ void playGame() {
           boolPlay = false;
       }
   }
+
+  //shiftRegisters();
+
 }
 
